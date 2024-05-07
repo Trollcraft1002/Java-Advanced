ec43d5be13140636729fe1057fac42a6b7c20c6a
S.Ivanov <trollcraft1002@gmail.com>
2024-05-08 01:28:05 +0300

    Finished the Lab

--- Java-Advanced/Streams-Files-and-Directories/Lab/SortLines.java
+++ Java-Advanced/Streams-Files-and-Directories/Lab/SortLines.java
@@ -1,23 +1,18 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class SortLines {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         String inputPath = "Java-Advanced/Streams-Files-and-Directories/Resources/input.txt";
         String outputPath = "Java-Advanced/Streams-Files-and-Directories/Resources/06.SortLinesOutput.txt";
 
 
-        try{
-            FileWriter fileWriter = new FileWriter(outputPath, true);
+        try(FileWriter fileWriter = new FileWriter(outputPath, true)){
+
             Files.lines(Path.of(inputPath)).sorted().forEach(line -> {
                 try {
                     fileWriter.write(line + "\n");
