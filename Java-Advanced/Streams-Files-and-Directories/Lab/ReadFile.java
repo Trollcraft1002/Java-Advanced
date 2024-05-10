3f6ceb7f3edab79434dc3d3aba06e710ff574d36
trollcraft <trollcraft1002@gmail.com>
2024-05-10 15:13:14 +0300

    formatted code

--- Java-Advanced/Streams-Files-and-Directories/Lab/ReadFile.java
+++ Java-Advanced/Streams-Files-and-Directories/Lab/ReadFile.java
@@ -4,19 +4,14 @@ import java.io.IOException;
 public class ReadFile {
     public static void main(String[] args) {
         String path = "Java-Advanced/Streams-Files-and-Directories/Resources/input.txt";
-        try (FileInputStream fileStream = new FileInputStream(path)){
+        try (FileInputStream fileStream = new FileInputStream(path)) {
             int bytesRead = fileStream.read();
 
-            while(bytesRead != -1){
-                System.out.print( Integer.toBinaryString(bytesRead) +" ");
+            while (bytesRead != -1) {
+                System.out.print(Integer.toBinaryString(bytesRead) + " ");
                 bytesRead = fileStream.read();
             }
-        }
-
-
-
-
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("File not found");
         }
     }
