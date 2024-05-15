3dcc975ed6a5c4af34acddbeb3fb6fd7cdae13d0
trollcraft <trollcraft1002@gmail.com>
2024-05-15 16:07:02 +0300

    Found how to use the Function<T,R>

--- Java-Advanced/Functional-Programming/Lab/SumNumbers.java
+++ Java-Advanced/Functional-Programming/Lab/SumNumbers.java
@@ -1,17 +1,20 @@
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 public class SumNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).toArray();
 
-        System.out.println(numbers);
+        //found how to use them
+        Function<int[], String> count = nums -> "Count = " + Arrays.stream(nums).count();
+        Function<int[], String> sum = nums -> "Sum = " + Arrays.stream(nums).sum();
+
+        System.out.println(count.apply(numbers));
+        System.out.println(sum.apply(numbers));
 
 
     }
