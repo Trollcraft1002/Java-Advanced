315656551304339e47fc2683e6c159e1abe9e3ac
S.Ivanov <trollcraft1002@gmail.com>
2024-05-16 23:37:16 +0300

    FilterByAge needs to be finished

--- Java-Advanced/Functional-Programming/Lab/FilterByAge.java
+++ Java-Advanced/Functional-Programming/Lab/FilterByAge.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FilterByAge {
     public static void main(String[] args) {
@@ -8,11 +11,15 @@ public class FilterByAge {
 
         int n = Integer.parseInt(scanner.nextLine());
         Map<String, Integer> users = new TreeMap<>();
-        while (n-- <= 0){
+        while (n-- != 0){
            String[] input = scanner.nextLine().split(", ");
             users.put(input[0], Integer.parseInt(input[1]));
         }
-        System.out.println(users.size());
+
+
+        for (Map.Entry<String, Integer> user : users.entrySet()){
+            System.out.println(user.getKey() + " " + user.getValue());
+        }
 
 
     }
